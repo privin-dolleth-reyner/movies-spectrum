3f7549688f5d4badf3d399dccdb538686ed04dc2
privindolleth <privin@okcredit.in>
2023-04-19 09:24:09 +0530

    Changed to grid layout

--- app/src/main/java/com/privin/movies/ui/NowPlayingFragment.kt
+++ app/src/main/java/com/privin/movies/ui/NowPlayingFragment.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
 import com.privin.movies.databinding.FragmentNowPlayingBinding
 
 class NowPlayingFragment : Fragment() {
@@ -42,7 +42,7 @@ class NowPlayingFragment : Fragment() {
 
     private fun setRecyclerView() {
         binding.listView.apply {
-            layoutManager = LinearLayoutManager(requireContext())
+            layoutManager = GridLayoutManager(requireContext(), 2)
             adapter = movieAdapter
         }
     }
